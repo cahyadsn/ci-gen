279fe2cf4f7258280197ca49484752da687aabc8
Cahya DSN <cahyadsn@gmail.com>
2018-12-11 00:46:53 +0700

    initial file

---
+++ config/gen.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Configuration for Gen controller
+ */
+
+ 
+/**
+ * Configure the templates directory
+ */ 
+$config['templates'] = 'views/gen_templates/';
+
+
+/**
+ * Tell the Gen what controller to extend
+ */
+$config['c_extends'] = 'CI';
+
+/**
+ * Tell the Gen what model to extend
+ */
+$config['mo_extends'] = 'CI';
+
+/**
+ * Tell the Gen what Migration to extend
+ */
+$config['mi_extends'] = 'CI';
